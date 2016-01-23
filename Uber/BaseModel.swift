698ebefe362347c51ed2a80c967c464a8aadd4ca
骆鹏飞 <luopengfei@luopengfeideMacBook-Pro.local>
2016-01-23 22:46:02 +0800

    uber project

---
+++ Uber/BaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseModel.swift
+//  Uber
+//
+//  Created by 骆鹏飞 on 16/1/23.
+//  Copyright (c) 2016年 Pengfei_Luo. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+   
+}
---
+++ Uber/RegisterOperation.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterOperation.swift
+//  Uber
+//
+//  Created by 骆鹏飞 on 16/1/23.
+//  Copyright (c) 2016年 Pengfei_Luo. All rights reserved.
+//
+
+import UIKit
+
+class RegisterOperation: BaseOperation {
+   
+}
